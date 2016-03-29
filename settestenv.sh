f1656a476608c909952197c70f37fe95a0deec6f
koshikawa <yuhji.koshikawa@town.biz>
2016-03-29 15:50:27 +0900

    Update settestenv.sh

--- settestenv.sh
+++ settestenv.sh
@@ -2,4 +2,3 @@
 
 PUBLICIP=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
 echo 'export PS1="\[\033[1;32m\][\u@\h(PUBLICIP) \W]\\$\[\033[0m\] "' | sed -e "s/PUBLICIP/${PUBLICIP}/g" > /etc/profile.d/testenv.sh
-source /etc/profile.d/testenv.sh
