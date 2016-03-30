877161e940c90729d838be0e179d00b14d424d07
koshikawa <yuhji.koshikawa@town.biz>
2016-03-30 13:43:56 +0900

    Update settestenv.sh

--- settestenv.sh
+++ settestenv.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-PUBLICIP=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
+PUBLICIP=`curl --connect-timeout 5 --max-time 10 http://169.254.169.254/latest/meta-data/public-ipv4`
+if [ "$PUBLICIP" = "" ]; then
+  PUBLICIP="TEST"
+fi
 echo 'export PS1="\[\033[1;32m\][\u@\h(PUBLICIP) \W]\\$\[\033[0m\] "' | sed -e "s/PUBLICIP/${PUBLICIP}/g" > /etc/profile.d/testenv.sh
