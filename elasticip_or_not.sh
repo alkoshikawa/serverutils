99f6c7a7ec3ac7f2b7446f32cc086a0175191266
koshikawa <yuhji.koshikawa@town.biz>
2016-05-27 14:57:05 +0900

    Create elasticip_or_not.sh

---
+++ elasticip_or_not.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+PUBLICIP=`curl --connect-timeout 5 --max-time 10 http://169.254.169.254/latest/meta-data/public-ipv4`
+
+if [ "$PUBLICIP" = "" ]; then
+  echo "can't get ip"
+  exit 1
+fi
+
+result=`aws ec2 describe-addresses | grep "$PUBLICIP" | wc -l`
+
+if [ $result -ne 0 ]; then
+  export PS1="\[\033[1;31m\][\u@\h \W]\\$\[\033[0m\] "
+fi
